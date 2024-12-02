df6bcae0d122c93a76cdb6f76fcd9d476572bc2e
Rahul Dey <rahuldey832000@gmail.com>
2024-12-02 21:29:52 +0530

    Add header and footer into layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,9 +5,34 @@ type IProps = {
 };
 
 function Layout({ children }: IProps) {
-  const header = <header></header>;
+  const header = (
+    <header>
+      <div>
+        <h1 className="text-gradient">CAFFIEND</h1>
+        <p>For Coffee Insatiate</p>
+      </div>
+      <button>
+        <p>Sign up free</p>
+        <i className="fa-solid fa-mug-hot"></i>
+      </button>
+    </header>
+  );
 
-  const footer = <footer></footer>;
+  const footer = (
+    <footer>
+      <p>
+        <span className="text-gradient">Caffiend</span> was made by{" "}
+        <a href="https://github.com/Rahul8320" target="_blank">
+          Rahul
+        </a>{" "}
+        using the{" "}
+        <a href="https://www.fantacss.com/" target="_blank">
+          FantaCSS
+        </a>{" "}
+        design library.
+      </p>
+    </footer>
+  );
 
   return (
     <>
