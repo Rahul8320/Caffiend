45bd4514efa77b4186c9e8a5fc21c91fa0a6fa86
Rahul Dey <rahuldey832000@gmail.com>
2024-12-10 22:51:33 +0530

    Update authentication form

--- src/components/Authentication.tsx
+++ src/components/Authentication.tsx
@@ -1,15 +1,44 @@
+import { useState } from "react";
+
 function Authentication() {
+  const [isRegistration, setIsRegistration] = useState<boolean>(false);
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [isAuthenticating, setIsAuthenticating] = useState<boolean>(false);
+
+  async function handleAuthentication() {}
+
   return (
     <>
-      <h2 className="sign-up-text">Sign Up / Login</h2>
-      <p>Sign in to your account!</p>
-      <input type="text" placeholder="Email" />
-      <input type="password" placeholder="********" />
-      <button type="submit">Login</button>
+      <h2 className="sign-up-text">{isRegistration ? "Sign Up" : "Login"}</h2>
+      <p>
+        {isRegistration ? "Create an account!" : "Sign in to your account!"}
+      </p>
+      <input
+        type="text"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Email"
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="********"
+      />
+      <button type="submit" onClick={handleAuthentication}>
+        {isRegistration ? "Sign Up" : "Login"}
+      </button>
       <hr />
       <div className="register-content">
-        <p>Don&apos;t have an account?</p>
-        <button>Sign up</button>
+        <p>
+          {isRegistration
+            ? "Already have an account?"
+            : "Don't have an account?"}
+        </p>
+        <button onClick={() => setIsRegistration(!isRegistration)}>
+          {isRegistration ? "Login" : "Sign up"}
+        </button>
       </div>
     </>
   );
